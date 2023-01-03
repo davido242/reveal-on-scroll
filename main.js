49ba33e98554f9c3392948b7076e2b299d9a70e2
Monday_David_Sarka <david_md@hamari.agency>
2023-01-03 12:46:12 +0100

    Added js scroll behavior

--- main.js
+++ main.js
@@ -2,9 +2,18 @@ window.addEventListener("scroll", hider);
 
 function hider() {
     var reveals = document.querySelectorAll('.reveal');
+    // var reveals = document.getElementsByClassName('reveal');
     
     for (var i = 0; i < reveals.length; i++) {
         var windowHeight = window.innerHeight;
         var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+
+        if(revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active');
+        }
+        else {
+            reveals[i].classList.remove('active');
+        }
     }
 }
